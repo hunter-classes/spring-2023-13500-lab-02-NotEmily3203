604736003db48e944c83def69dcb3f513c70d3ad
NotEmily3203 <linemily62@gmail.com>
2023-02-02 23:11:53 -0500

    completed lab D file

---
+++ fibonacci.cpp
@@ -0,0 +1,14 @@
+//Name: Emily Lin; Github: NotEmily
+#include <iostream>
+int main(){
+    int fib[60];
+    fib[0] = 0;
+    fib[1] = 1;
+    for(int i=2;i<60;i++){
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+    for(int i=0;i<60;i++){
+        std::cout<<fib[i]<<std::endl;
+    }
+    return 0;
+}
