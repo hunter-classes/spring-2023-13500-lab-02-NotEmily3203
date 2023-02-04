e6bc8df48ec61c0d260ad62d0db21e1c7e79da10
NotEmily3203 <linemily62@gmail.com>
2023-02-03 23:04:11 -0500

    Created file functionality and linked files

---
+++ funcs.h
@@ -0,0 +1,3 @@
+//This file contains the function declaration
+#pragma once
+void print_interval(int,int);
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "funcs.h"
+int main(){
+    print_interval(-5,10);
+    return 0;
+}
