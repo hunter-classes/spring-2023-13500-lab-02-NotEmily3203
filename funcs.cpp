7fbd819b697ad2d2f2fccdf7b180f13b9d7ef152
NotEmily3203 <linemily62@gmail.com>
2023-02-03 23:07:35 -0500

    Added more tests and improved readability

--- funcs.cpp
+++ funcs.cpp
@@ -4,4 +4,5 @@ void print_interval(int L,int U){
     for(int i=L;i<U;i++){
         std::cout<<i<<" ";
     }
+    std::cout<<"\n";
 }
--- main.cpp
+++ main.cpp
@@ -2,5 +2,8 @@
 #include "funcs.h"
 int main(){
     print_interval(-5,10);
+    print_interval(-7,8);
+    print_interval(0,20);
+    print_interval(10,15);
     return 0;
 }
